64cd77c725de836c3c99d9f76357c4afd134079e
Bryan Reamillo <bryanreamillo2403@gmail.com>
2026-03-26 14:03:09 +0800

    feat: enhance insertAt and removeAt methods for LinkedList

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -132,32 +132,70 @@ export class LinkedList {
             if (idx < 0 || idx > this.size()) throw new RangeError("Not in range");
         } catch (error) {
             console.log(error)
+            return;
         }
 
         let currentNode = this.head;
         let counter = 0;
         let prevNode = null;
 
-        while (currentNode) {
-            if (counter === idx) break; // currentNode is target
-            prevNode = currentNode;
-            counter++;
-            currentNode = currentNode.nextNode;
+        if (idx === 0) {
+            const oldHead = this.head;
+            const firstHead = new Node(values[0]);
+            this.head = firstHead;
+            let lastInserted = firstHead;
+
+            if (values.length > 1) {
+                for (const value of values.slice(1)) {
+
+                    lastInserted.nextNode = new Node(value);
+                    lastInserted = lastInserted.nextNode;
+
+                }
+            }
+            lastInserted.nextNode = oldHead;
+        } else {
+            while (currentNode) {
+                if (counter === idx) break; // currentNode is target
+                prevNode = currentNode;
+                counter++;
+                currentNode = currentNode.nextNode;
+            }
+            prevNode.nextNode = null;
+
+            let lastInserted = prevNode;
+            for (let i = 0; i < values.length; i++) {
+
+                lastInserted.nextNode = new Node(values[i]);
+                lastInserted = lastInserted.nextNode;
+
+            }
+
+            lastInserted.nextNode = currentNode;
         }
-
-        prevNode.nextNode = null;
-
-        let lastInserted = prevNode;
-        for (let i = 0; i < values.length; i++) {
-
-            lastInserted.nextNode = new Node(values[i]);
-            lastInserted = lastInserted.nextNode;
-
-        }
-
-        lastInserted.nextNode = currentNode;
-
 
         return undefined;
+    }
+
+    removeAt(idx) {
+        if (!this.head) return null;
+
+        let currentNode = this.head;
+        let counter = 0;
+
+        if (idx === 0) {
+            this.head = currentNode.nextNode;
+        } else {
+            let prevNode = null;
+
+            while (currentNode) {
+                if (idx === counter) break;
+                prevNode = currentNode;
+                counter++;
+                currentNode = currentNode.nextNode;
+            }
+
+            prevNode.nextNode = currentNode.nextNode;
+        }
     }
 }
